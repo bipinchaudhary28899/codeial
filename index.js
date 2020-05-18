3e3cb353a9f25d171a60e37dacadd4f9ea9bc080
bipinchaudhary28899 <45562225+bipinchaudhary28899@users.noreply.github.com>
2020-05-18 08:33:48 +0530

    Social website
    added signup option and commenting

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cookieParser=require('cookie-parser');
+const port=8000;
+const app=express();
+const expressLayouts=require('express-ejs-layouts');
+const db=require('./config/mongoose');
+
+
+app.use(express.urlencoded());
+
+app.use(cookieParser());
+
+app.use(expressLayouts);
+app.use(express.static('./assets'));
+
+//extract styles and scripts from sub pages into the layout
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
+//use express router
+ app.use('/',require('./routes'));
+ app.set('view engine','ejs');
+app.set('views','./views');
+
+app.listen(port,function(err){
+    if(err){
+        console.log(`error in running the server:${err}`);
+    }
+    console.log(`server is running on port no.:${port}`);
+});
